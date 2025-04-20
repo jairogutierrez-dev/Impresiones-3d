7161a9a0e8d087d2e459e69fd6e2d7ab06692582
unknown <187451352+jairogutierrez-dev@users.noreply.github.com>
2025-04-20 23:13:21 +0200

    Eliminado config_env.php del seguimiento de Git

--- config_env.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'DB_HOST' => 'sql312.infinityfree.com', // Este es el host que te da InfinityFree
-  'DB_NAME' => 'if0_38702516_tienda3d',
-  'DB_USER' => 'if0_38702516',
-  'DB_PASS' => 'KfG43UJ4LaPluC'
-];
